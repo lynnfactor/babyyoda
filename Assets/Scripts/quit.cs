a0c1967470bf319c4e5c21f20ea94e82ddce1e1d
Aubrey Lynn Isaacman <aubrey@lynnfactor.com>
2020-11-21 15:32:07 -0800

    start game, restart level, and quit work

---
+++ Assets/Scripts/quit.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/* created by Aubrey Isaacman
+ *
+ * this script lets you quit the game and restart the scene
+*/
+
+public class quit : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        } else if (Input.GetKeyDown("escape"))
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ Assets/Scripts/start.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+/* created by Aubrey Isaacman
+ *
+ * this script lets you start, quit, and restart the game
+*/
+public class start : MonoBehaviour
+{
+
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/AssetBundles/scenes");
+        scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+
+        if(Input.GetKeyDown("space"))
+        {
+            SceneManager.LoadScene(scenePaths[1], LoadSceneMode.Single);
+        } 
+    }
+}
